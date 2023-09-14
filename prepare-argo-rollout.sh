0a040920ffa90b3c479bbcabe02e0f9eaf5175a3
Thiago Saraiva <thiiagowilliam@gmail.com>
2023-09-14 10:13:08 -0300

    update rollout

--- prepare-argo-rollout.sh
+++
@@ -1,9 +0,0 @@
-k3d cluster create prod --agents 2 --volume /Users/xlp67/devops/volume:/storage
-k taint node k3d-prod-server-0 key1=value1:NoSchedule
-helmfile apply -l name=argocd
-helmfile apply -l name=argocd-rollouts
-kaf ~/Developer/monitoring/config/kube-state-metrics
-kaf ~/Developer/monitoring/config/prometheus
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-kaf application.yaml
-k port-forward svc/argocd-server -n argocd 8080:80
